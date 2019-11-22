4620d7767281d664d3994a0bb0464650038c99d5
gg <andrew.peter.bryan@gmail.com>
2019-11-22 08:24:15 -0600

    make CVaR... files callable, not main

--- src/CVaRHistorical.cpp
+++ src/CVaRHistorical.cpp
@@ -1,15 +1,12 @@
-//test_ptf_mc_var.cpp
+// Portfolio VaR through MC simulation
+// from test_ptf_mc_var.cpp
 
-//test portfolio VaR through MC simulation
-
-#include <iostream>
 #include <string>
-#include <fstream>
-#include <sstream>
 #include <vector>
-#include<memory>
 
 #include <Eigen/Dense>
+
+#include "ptf_mc_var.h"
 
 #include "compute_returns_eigen.h"
 #include "compute_var.h"
@@ -23,100 +20,50 @@
 #include <boost/math/special_functions/erf.hpp>
 using boost::math::erfc_inv;
 
+namespace RiskJS {
+
+
 using namespace Eigen;
 using namespace std;
 
-void readCSV(std::istream &input, std::vector< std::vector<std::string> > &output)
-//https://www.gamedev.net/topic/444193-c-how-to-load-in-a-csv-file/
-{
-	std::string csvLine;
-	// read every line from the stream
-	while( std::getline(input, csvLine) )
-	{
-		std::istringstream csvStream(csvLine);
-		std::vector<std::string> csvColumn;
-		std::string csvElement;
-		// read every element from the line that is seperated by commas
-		// and put it into the vector or strings
-		while( std::getline(csvStream, csvElement, ',') )
-		{
-			csvColumn.push_back(csvElement);
-		}
-		output.push_back(csvColumn);
-	}
-}
 
-
-int main()
-{
-
-    try{
-
-    // Read daily price history for crypto assets, in USD
-    //Date,EOS,DAPP
-    //2018-11-19,3.5,0.5
-    //2018-11-20,3.6,0.6 
-
-	std::fstream file("/home/gg/VaR/test/data.csv", ios::in);
-	if(!file.is_open())
-	{
-		std::cout << "File not found!\n";
-		return 1;
-	}
-	// typedef to save typing for the following object
-	typedef std::vector< std::vector<std::string> > csvVector;
-	csvVector csvData;
-
-	readCSV(file, csvData);
-
-    //test
-    for(size_t i = 0;i < 5; ++i){
-        for(size_t j = 0;j < csvData[i].size();++j){
-            cout << csvData[i][j] << '\t';
-
-        }
-
-        cout << endl;
-    }
-    cout << endl;
-
+double CVaRHistorical(priceData data) {
     // Remove lines with missing values
-
-    size_t n(csvData.size() - 1);
-    size_t m(csvData[0].size() - 1);
+    size_t n(data.size() - 1);
+    size_t m(data[0].size() - 1);
 
     Mat _prices;
     _prices.resize(m,Vec(n));
 
     for(size_t i = 1;i < n+1;++i){
-        for(size_t j = 1;j < csvData[i].size();++j){
-            std::string tmp = csvData[i][j];
+        for(size_t j = 1;j < data[i].size();++j){
+            string tmp = data[i][j];
             if(tmp.empty()){
                 _prices[j-1][i-1] = 99999.;
             }
             else{
-                _prices[j-1][i-1] = std::stod(tmp);
+                _prices[j-1][i-1] = stod(tmp);
             }
         }
     }
 
-    std::vector<std::string> indexNames(csvData[0].size() - 1);
+    vector<string> indexNames(data[0].size() - 1);
 
-    for(size_t i = 1;i < csvData[0].size();++i){
-        indexNames[i-1] = csvData[0][i];
+    for(size_t i = 1;i < data[0].size();++i){
+        indexNames[i-1] = data[0][i];
     }
 
-	//Remove missing values to compute trailling returns
-    //Asynchornous time series. Shift to the next value
+		// Remove missing values to compute trailling returns
+    // Asynchornous time series. Shift to the next value
     Mat prices;
     prices.resize(m,Vec(0));
 
-	for(size_t i = 0;i < _prices.size();++i){
-        for(size_t j = 0;j < _prices[i].size();++j){
-            if(!((_prices[i][j] == 99999) || (_prices[i][j] == 0)))
-                prices[i].push_back(_prices[i][j]);
-        }
-	}
+		for(size_t i = 0;i < _prices.size();++i){
+	        for(size_t j = 0;j < _prices[i].size();++j){
+	            if(!((_prices[i][j] == 99999) || (_prices[i][j] == 0)))
+	                prices[i].push_back(_prices[i][j]);
+	        }
+		}
     unsigned int windowsize = prices[0].size()-2;
     std::shared_ptr<ComputeReturn> cr(new ComputeReturn(prices,1,windowsize,true));
 
@@ -140,12 +87,9 @@ int main()
     double CVaRHistorical = model(0,ptf->getReturns()); 
     cout << "CVaRHistorical: " << CVaRHistorical << endl;
 
-    return 0;
+    return CVaRHistorical;
 
-    } catch (const std::exception& e) { // caught by reference to base
-        std::cout << " a standard exception was caught, with message '"
-                  << e.what() << "'\n";
-    }
+    } 
 
 }
 
